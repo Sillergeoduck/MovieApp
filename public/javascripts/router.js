260710bfee9d49048e5c84f73609bfc3dde65f59
sillergeoduck <lochan.rajeev1@gmail.com>
2015-12-18 15:30:07 -0800

    Changes for movie api

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -2,15 +2,23 @@
  * Created by Rajeev on 12/17/15.
  */
 //var myApp = angular.module("movieApp", ['ngRoute']);
-myApp.config(['$routeProvider',function($routeProvider){
-    $routeProvider.when('/movieList',{
-            templateUrl: '/MovieList.ejs',
-        controller: 'MovieController'
-    }).when('/upcoming',{
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/movieList', {
         templateUrl: '/MovieList.ejs',
         controller: 'MovieController'
-    }).
-    otherwise({
+    }).when('/movie/nowplaying', {
+        templateUrl: '/MovieList.ejs',
+        controller: 'MovieController'
+    }).when('/movie/upcoming', {
+        templateUrl: '/MovieList.ejs',
+        controller: 'MovieController'
+    }).when('/movie/popular', {
+        templateUrl: '/MovieList.ejs',
+        controller: 'MovieController'
+    }).when('/movie/search', {
+            templateUrl: '/MovieList.ejs',
+            controller: 'MovieController'
+        }).otherwise({
         redirectTo: 'ANTMAN'
     })
 }]);
